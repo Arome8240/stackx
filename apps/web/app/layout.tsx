18ef92561e8294ce8807f3ee5e8c3df5d3e05dd4
Arome8240 <arome8240@gmail.com>
2026-03-07 16:19:47 +0100

    fd

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,15 @@
 import './globals.css';
 import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Staxial',
   description:
-    'A Social DeFi platform built on the Stacks blockchain that combines social networking with decentralized finance primitives.'
+    'A Social DeFi platform built on the Stacks blockchain that combines social networking with decentralized finance primitives.',
+  other: {
+    'talentapp:project_verification':
+      '82210c71536402a53da29e4729989fcd6eae087834f4c0e6d189c3f0ade7000bb1e364e4bdeeeec362e5bea2ee7098a020d070495f73ab6331a148d54041c174'
+  }
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
